70865b47f5f524296034ed11bda532bac750a5a2
ABIramiPM <abiramipm99@gmail.com>
2026-08-14 12:10:55 +0530

    add tourism_project/model_building/prep.py

---
+++ tourism_project/model_building/prep.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("tourism_project/data/tourism.csv")
+
+#Customer ID is only an identifier so it will be droped.
+df.drop(columns=["CustomerID"], inplace=True)
+
+
+X = df.drop(columns=["prodTaken"])
+y = df["ProdTaken"]
+
+# Stratify=y keeps the (imbalaced) purchase ratio consistent across splits
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    X, y , test_size=0.2, random_state=42, stratify=y
+)
+
+
+Xtrain.to_csv("Xtrain.csv", index=False)
+Xtest.to_csv("Xtest.csv", index=False)
+ytrain.to_csv("ytrain.csv", index=False)
+ytest.to_csv("ytest.csv", index=False)
+
+
+print("Data prepared: train/test splits written.")
+print("Type values kept as:", sorted(X["Type"].unique()))
